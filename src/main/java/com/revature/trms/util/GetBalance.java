7b0c397aa49c9b76247af89b28aef092aa8e23fa
rhirsch14 <rhirsch14@gmail.com>
2017-11-05 16:58:09 -0500

    2 question marks

--- src/main/java/com/revature/trms/util/GetBalance.java
+++ src/main/java/com/revature/trms/util/GetBalance.java
@@ -9,9 +9,29 @@ import com.revature.trms.database.dao.RequestDAO;
 import com.revature.trms.database.dao.RequestDAOImpl;
 import com.revature.trms.models.Request;
 
+import com.revature.trms.database.dao.EmployeeDAO;
+import com.revature.trms.database.dao.EmployeeDAOImpl;
+import com.revature.trms.models.Employee;
+
+
 
 public class GetBalance {
 
-	
-	
+	public static double getPendingBalance(Employee emp)
+	{
+		
+		return 0;
+		
+	}
+	public static double getApprovedBalance(Employee emp)
+	{
+		
+		return 0;
+		
+	}
+	public static double getTotalBalance(Employee emp)
+	{
+		double total = (1000 - getApprovedBalance(emp) - getPendingBalance(emp));
+		return total;
+	}
 }
